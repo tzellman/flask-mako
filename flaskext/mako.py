f2e88d0968caf2ef6cb80adc1c95e0f385e2d789
Tom Zellman <tzellman@gmail.com>
2011-03-03 14:05:41 -0500

    tack the lookup onto the _request_ctx_stack.top rather than g

--- flaskext/mako.py
+++ flaskext/mako.py
@@ -2,28 +2,40 @@
 from __future__ import absolute_import
 from mako.lookup import TemplateLookup
 from mako.template import Template
-from flask import g, request, session, get_flashed_messages, url_for
+from flask import request, session, get_flashed_messages, url_for, g
+from flask import _request_ctx_stack
 
-def init_mako(app):
+
+def init_mako(app, **kw):
     app = app
-    get = app.config.get
     
-    dirOps = filter(lambda x: x in app.config,
-                    map(lambda x: 'MAKO_%s' % x, ('DIRS', 'DIRECTORIES', 'DIR', 'DIRECTORY')))
-    dirs = len(dirOps) and get(dirOps[0]) or ['.']
+    def get_first(dicts, keys, default=None):
+        # look in one or more dictionaries returning the first found value
+        for d in dicts:
+            found = filter(lambda x: x in d, keys)
+            if found:
+                return d[found[0]]
+        return default
+    
+    dirs = get_first([kw, app.config],
+                     map(lambda x: 'MAKO_%s' % x, ('DIRS', 'DIRECTORIES', 'DIR', 'DIRECTORY')),
+                     default='.')
     if type(dirs) == str:
         dirs = dirs.split(' ')
+    
+    get = app.config.get
     lookup = TemplateLookup(directories=dirs,
                             input_encoding=get('MAKO_INPUT_ENCODING', 'utf-8'),
                             output_encoding=get('MAKO_OUTPUT_ENCODING', 'utf-8'),
                             module_directory=get('MAKO_CACHEDIR', None),
                             collection_size=get('MAKO_CACHESIZE', None),
                             imports=get('MAKO_IMPORTS', None))
-
+    
     @app.before_request
     def before_request():
-        g._makoLookup = lookup
-
+        _request_ctx_stack.top._mako_lookup = lookup
+    
+    return app
 
 def render_template(path, **kw):
     """
@@ -31,10 +43,10 @@ def render_template(path, **kw):
     If mako does not exist, it falls back on the built-in (Jinja2)
     """
     try:
-        return g._makoLookup.get_template(path).render(g=g, request=request,
-                                                       get_flashed_messages=get_flashed_messages,
-                                                       session=session,
-                                                       url_for=url_for, **kw)
+        render = _request_ctx_stack.top._mako_lookup.get_template(path).render
+        return render(g=g, request=request,
+                      get_flashed_messages=get_flashed_messages,
+                      session=session, url_for=url_for, **kw)
     except AttributeError:
         from flask import render_template
         return render_template(path, **kw)
